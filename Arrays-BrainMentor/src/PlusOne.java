d50110cb7ce10cc6c18a810d92f3df19f5cdc4cf
Rohit Kohli <kohlirohit2428@gmail.com>
2026-03-28 09:39:16 +0530

    More Practice Questions

---
+++ Arrays-BrainMentor/src/PlusOne.java
@@ -0,0 +1,24 @@
+public class PlusOne {
+
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {1, 2, 3};
+        PlusOne plusOne = new PlusOne();
+        int[] result = plusOne.plusOne(digits);
+        for(int digit : result) {
+            System.out.print(digit + " ");
+        }
+    }
+}
