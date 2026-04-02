beecf46a4ed75efb3941370cf15a930daa2a96dd
Rohit Kohli <kohlirohit2428@gmail.com>
2026-04-02 21:43:12 +0530

    More Practice Questions

---
+++ Arrays-BrainMentor/src/ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+public class ContainerWithMostWater {
+
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int w = right - left;
+            maxArea = Math.max(maxArea, h * w);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+}
+        return maxArea;
+    }
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(height)); // Output: 49
+    }
+}
