7179aad6d06f20cf1837fe8deacf7731ac7b2c51
Rohit Kohli <kohlirohit2428@gmail.com>
2026-04-16 22:14:23 +0530

    More Practice Questions

---
+++ Arrays-BrainMentor/src/MergeInterval.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class MergeInterval {
+
+    public static int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][];
+        }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        merged.add(currentInterval);
+        for (int[] interval : intervals) {
+            if (interval[0] <= currentInterval[1]) {
+                currentInterval[1] = Math.max(currentInterval[1], interval[1]);
+            } else {
+                currentInterval = interval;
+                merged.add(currentInterval);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+    public static void main(String[] args) {
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] merged = merge(intervals);
+        for (int[] interval : merged) {
+            System.out.println("[" + interval[0] + ", " + interval[1] + "]");
+        }
+    }
+}
---
+++ Arrays-BrainMentor/src/SortAnArray.java
@@ -0,0 +1,22 @@
+public class SortAnArray {
+
+    public static void sortArray(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        sortArray(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
