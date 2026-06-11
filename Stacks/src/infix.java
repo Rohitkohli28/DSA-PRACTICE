a172f91ea9d3f43352bc75c66d68a6d0cf9a455a
Rohit Kohli <kohlirohit2428@gmail.com>
2026-06-11 09:15:26 +0530

    More Practice Questions

---
+++ Stacks/src/infix.java
@@ -0,0 +1,59 @@
+import java.util.Stack;
+import java.util.*;
+
+public class infix {
+
+    public static void main(String[] args) {
+        String str = "9-5+3*4/6";
+        Stack<Integer> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            int ascii = (int)ch;
+            // 0 --> 48 and 9 --> 57
+            if (ascii >= 48 && ascii <= 57){
+                val.push(ascii-48); // to convert char to int
+            } else if(op.size() == 0){
+                op.push(ch);
+            } else {
+                if(ch == '+' || ch == '-'){
+                    // Work
+                    int v2 = val.pop();
+                    int v1 = val.pop();
+                    if(op.peek() == '-') val.push(v1-v2);
+                    if (op.peek() == '+') val.push(v1+v2);
+                    if (op.peek() == '*') val.push(v1*v2);
+                    if (op.peek() == '/') val.push(v1/v2);
+                    op.pop();
+                    op.push(ch);
+                }
+
+                if(ch == '*' || ch == '/'){
+                    if(op.peek() == '*' || op.peek() == '/') {
+                        // Work
+                        int v2 = val.pop();
+                        int v1 = val.pop();
+                        if (op.peek() == '*') val.push(v1 * v2);
+                        if (op.peek() == '/') val.push(v1 / v2);
+                        op.pop();
+                    }
+                    else op.push(ch);
+                }
+            }
+
+        }
+
+        while(val.size() > 1){
+            int v2 = val.pop();
+            int v1 = val.pop();
+            if (op.peek() == '-') val.push(v1-v2);
+            if (op.peek() == '+') val.push(v1+v2);
+            if (op.peek() == '*') val.push(v1*v2);
+            if (op.peek() == '/') val.push(v1/v2);
+            op.pop();
+         }
+
+         System.out.println(val.peek());
+        }
+    }
