ab0eceafbb17903eb613f176ee645958da08896b
Rohit Kohli <kohlirohit2428@gmail.com>
2026-06-11 21:24:35 +0530

    More Practice Questions

---
+++ Stacks/src/InfixToPostfix.java
@@ -0,0 +1,66 @@
+import java.util.Stack;
+
+public class InfixToPostfix {
+    public static void main(String[] args) {
+        String infix = "9-(5+3)*4/6";
+        System.out.println(infix);
+        Stack<String> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for(int i=0; i<infix.length(); i++){
+            char ch = infix.charAt(i);
+            int ascii = (int)ch;
+            // 0 --> 48 and 9 --> 57
+            if (ascii >= 48 && ascii <= 57){ // 0 to 9
+                String s = "" + ch;
+                val.push(s);
+            } else if(op.size() == 0 || ch == '(' || op.peek() == '('){
+                op.push(ch);
+            } else if (ch == ')'){
+                while(op.peek() != '('){
+                    // Work
+                    String v2 = val.pop();
+                    String v1 = val.pop();
+                    char o = op.pop();
+                    String t =  v1 + v2 + o;
+                    val.push(t);
+                }
+                op.pop(); // to pop '('
+            }
+            else {
+                if(ch == '+' || ch == '-'){
+                    // Work
+                    String v2 = val.pop();
+                    String v1 = val.pop();
+                    char o = op.pop();
+                    String t = v1 + v2 + o;
+                    val.push(t);
+                    op.push(ch);
+                }
+
+                if(ch == '*' || ch == '/'){
+                    if(op.peek() == '*' || op.peek() == '/') {
+                        String v2 = val.pop();
+                        String v1 = val.pop();
+                        char o = op.pop();
+                        String t = v1 + v2 + o;
+                        val.push(t);
+                        op.push(ch);
+                    }
+                    else op.push(ch);
+                }
+            }
+
+        }
+
+        while(!op.isEmpty()){
+            String v2 = val.pop();
+            String v1 = val.pop();
+            char o = op.pop();
+            String t = v1 + v2 + o;
+            val.push(t);
+        }
+
+        System.out.println(val.peek());
+    }
+}
