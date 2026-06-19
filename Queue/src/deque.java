b7f107f273431acfadf62e68caa4871a3e45a180
Rohit Kohli <kohlirohit2428@gmail.com>
2026-06-19 12:01:48 +0530

    More Practice Questions

---
+++ Queue/src/deque.java
@@ -0,0 +1,17 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+
+public class deque {
+
+    public static void main(String[] args) {
+        Deque<Integer> dq = new LinkedList<>();
+        dq.addLast(1);
+        dq.addLast(2);
+        dq.addLast(3);
+        dq.addLast(4);
+        dq.addLast(5);
+        dq.addFirst(6);
+        System.out.println(dq);
+    }
+}
